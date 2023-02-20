aa340142ff72727963b1f5f1b4df4c314e4482b7
iZachyno <mohaumoleli@gmail.com>
2023-02-20 14:36:22 +0000

    first

--- src/main/java/com/example/uipalettes/Flag.java
+++ src/main/java/com/example/uipalettes/Flag.java
@@ -3,34 +3,92 @@ package com.example.uipalettes;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
-public class Flag
+public class Flag implements Initializable
 {
     @FXML
     ImageView imgFlag;
     @FXML
-    Label countryName;
+    Label countryName, countryInfo;
     @FXML
     Button btnBack;
 
-    void setData(ImageView image, StringBuilder fileName)
-    {
+    void setData(ImageView image, StringBuilder fileName) throws FileNotFoundException {
         fileName.deleteCharAt(fileName.length() - 1);
         fileName.deleteCharAt(fileName.length() - 1);
         fileName.deleteCharAt(fileName.length() - 1);
         fileName.deleteCharAt(fileName.length() - 1);
         imgFlag.setImage(image.getImage());
         countryName.setText(fileName.toString());
+        setCountryInfo(fileName);
+    }
+    void setCountryInfo(StringBuilder countryName) throws FileNotFoundException {
+        File file = new File("L:\\Database\\Cpp\\current_output.txt");
+
+        for (int i = 0; i < countryName.length(); i++)
+        {
+            if (countryName.charAt(i) == ' ')
+            {
+                countryName.setCharAt(i, '-');
+            }
+        }
+        countryInfo.setText(countryName.toString());
+
+        Scanner scanner = new Scanner(file);
+
+        String name;
+
+        while(scanner.hasNextLine())
+        {
+            name = scanner.nextLine();
+            String longString = name;
+            name = wordSplit(name, 2);
+            StringBuilder newS = new StringBuilder(this.countryInfo.getText());
+            String[] temp = wordSplit1(newS.toString());
+            newS = new StringBuilder();
+            for (String s : temp) {
+                newS.append(s);
+            }
+            //System.out.println(name + " | " + newS);
+            if (name.equals(newS.toString()))
+            {
+                String r = "";
+                r += countryName + "\n\n" + wordSplit(longString, 0) + "\n\n" + wordSplit(longString, 1) + "\n\n" +
+                        wordSplit(longString, 3) + "\n\n" + wordSplit(longString, 4) + "\n\n" + wordSplit(longString, 5) + "\n\n" +
+                        wordSplit(longString, 6) + "\n\n" + wordSplit(longString, 7) + "\n\n" + wordSplit(longString, 8);
+                countryInfo.setText(r);
+            }
+        }
+    }
+    String wordSplit(String s, int val)
+    {
+        String[] words = s.split("\t");
+
+        String[] words1 = words[val].split("-");
+        StringBuilder Return = new StringBuilder();
+
+        for (String value : words1) {
+            Return.append(value);
+        }
+        return Return.toString();
+    }
+    String[] wordSplit1(String s)
+    {
+        return s.split("\\W+");
     }
     @FXML
     void goBack(ActionEvent ac) throws IOException
@@ -41,6 +99,22 @@ public class Flag
         stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void backIn()
+    {
+        btnBack.setStyle("-fx-background-color:  #111111; -fx-border-color:  #111; -fx-background-radius: 8px; -fx-border-radius: 8px; -fx-text-fill: ghostwhite;");
+    }
+
+    public void backOut()
+    {
+        btnBack.setStyle("-fx-background-color:  #11111115; -fx-border-color:  #111; -fx-text-fill: #111;");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
     }
 }
 
--- src/main/java/com/example/uipalettes/MainController.java
+++ src/main/java/com/example/uipalettes/MainController.java
@@ -1,7 +1,5 @@
 package com.example.uipalettes;
 
-import javafx.animation.ScaleTransition;
-import javafx.animation.Transition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +9,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,9 +19,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
 
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -32,6 +28,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    public MenuBar menuBar;
 
     //Declarations
 
@@ -111,8 +108,6 @@ public class MainController implements Initializable {
             stage.setY(event12.getScreenY() - yOffset);
         });
     }
-
-
     @FXML
     protected void fExit()
     {
@@ -202,7 +197,7 @@ public class MainController implements Initializable {
     }
     private void loadCanvas()
     {
-        String path = "C:\\Users\\Sarah\\IdeaProjects\\Flags\\src\\main\\resources\\com\\example\\uipalettes\\img";
+        String path = "C:\\Users\\Sarah\\Videos\\Anime\\Flags";
 
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
@@ -241,10 +236,8 @@ public class MainController implements Initializable {
                 imageCard.setStyle("-fx-background-color: #111; -fx-background-radius: 8px;");
                 fileName.setStyle("-fx-text-fill: #999; -fx-background-color: transparent; -fx-border-color: transparent");
             });
-
             canvas.getChildren().addAll(imageCard);
         }
-
     }
     private ImageView createImageView(final File imageFile) {
         // DEFAULT_THUMBNAIL_WIDTH is a constant you need to define
@@ -266,7 +259,6 @@ public class MainController implements Initializable {
                     if(mouseEvent.getClickCount() == 2){
                         try
                         {
-
                             FXMLLoader fxmlLoader = new FXMLLoader();
                             fxmlLoader.setLocation(getClass().getResource("flag.fxml"));
                             BorderPane BP = fxmlLoader.load();
@@ -274,11 +266,11 @@ public class MainController implements Initializable {
                             StringBuilder FileName = new StringBuilder(imageFile.getName());
                             cardController.setData(finalImageView, FileName);
                             borderHome.setCenter(BP);
-
-                        } catch (Exception e) {
+                        }
+                        catch (Exception e)
+                        {
                             e.printStackTrace();
                         }
-
                     }
                 }
             });
@@ -327,249 +319,4 @@ public class MainController implements Initializable {
         setUpTooltips();
         loadCanvas();
     }
-
 }
-
-/*
-
-package com.example.uipalettes;
-
-import ColorCard.ColorClass;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-
-import java.io.IOException;
-import java.net.URL;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
-public class GeneratePalettes implements Initializable
-{
-    public ScrollPane scrollPaneTrending;
-    @FXML
-    BorderPane homePane;
-    @FXML
-    HBox trendingColorsLayout;
-    @FXML
-    GridPane paletteList;
-
-    private List<ColorClass> recentlyAdded()
-    {
-        List<ColorClass> ls = new ArrayList<>();
-        ColorClass colorClass;
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("1D1E18");
-        colorClass.setColor2("6B8F71");
-        colorClass.setColor3("AAD2BA");
-        colorClass.setColor4("D9FFF5");
-        colorClass.setColor5("B9F5D8");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("4E5166");
-        colorClass.setColor2("C98686");
-        colorClass.setColor3("F2B880");
-        colorClass.setColor4("FFF4EC");
-        colorClass.setColor5("E7CFBC");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("966B9D");
-        colorClass.setColor2("7C90A0");
-        colorClass.setColor3("B5AA9D");
-        colorClass.setColor4("B9B7A7");
-        colorClass.setColor5("747274");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("C5D1EB");
-        colorClass.setColor2("92AFD7");
-        colorClass.setColor3("5A7684");
-        colorClass.setColor4("395B50");
-        colorClass.setColor5("1F2F16");
-
-        ls.add(colorClass);
-
-        return ls;
-    }
-    private List<ColorClass> mainPalettes()
-    {
-        List<ColorClass> ls = new ArrayList<>();
-        ColorClass colorClass = new ColorClass();
-        colorClass.setColor1("817F82");
-        colorClass.setColor2("AE8CA3");
-        colorClass.setColor3("E7DFC6");
-        colorClass.setColor4("6B8F71");
-        colorClass.setColor5("D9FFF5");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("1D1E18");
-        colorClass.setColor2("6B8F71");
-        colorClass.setColor3("AAD2BA");
-        colorClass.setColor4("D9FFF5");
-        colorClass.setColor5("B9F5D8");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("4E5166");
-        colorClass.setColor2("C98686");
-        colorClass.setColor3("F2B880");
-        colorClass.setColor4("FFF4EC");
-        colorClass.setColor5("E7CFBC");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("966B9D");
-        colorClass.setColor2("7C90A0");
-        colorClass.setColor3("B5AA9D");
-        colorClass.setColor4("B9B7A7");
-        colorClass.setColor5("747274");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("571F4E");
-        colorClass.setColor2("5D5179");
-        colorClass.setColor3("4F759B");
-        colorClass.setColor4("92C9B1");
-        colorClass.setColor5("A2FAA3");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("C5D1EB");
-        colorClass.setColor2("92AFD7");
-        colorClass.setColor3("5A7684");
-        colorClass.setColor4("395B50");
-        colorClass.setColor5("1F2F16");
-
-        ls.add(colorClass);
-
-        colorClass.setColor1("817F82");
-        colorClass.setColor2("AE8CA3");
-        colorClass.setColor3("E7DFC6");
-        colorClass.setColor4("6B8F71");
-        colorClass.setColor5("D9FFF5");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("1D1E18");
-        colorClass.setColor2("6B8F71");
-        colorClass.setColor3("AAD2BA");
-        colorClass.setColor4("D9FFF5");
-        colorClass.setColor5("B9F5D8");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("4E5166");
-        colorClass.setColor2("C98686");
-        colorClass.setColor3("F2B880");
-        colorClass.setColor4("FFF4EC");
-        colorClass.setColor5("E7CFBC");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("966B9D");
-        colorClass.setColor2("7C90A0");
-        colorClass.setColor3("B5AA9D");
-        colorClass.setColor4("B9B7A7");
-        colorClass.setColor5("747274");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("571F4E");
-        colorClass.setColor2("5D5179");
-        colorClass.setColor3("4F759B");
-        colorClass.setColor4("92C9B1");
-        colorClass.setColor5("A2FAA3");
-
-        ls.add(colorClass);
-
-        colorClass = new ColorClass();
-        colorClass.setColor1("C5D1EB");
-        colorClass.setColor2("92AFD7");
-        colorClass.setColor3("5A7684");
-        colorClass.setColor4("395B50");
-        colorClass.setColor5("1F2F16");
-
-        ls.add(colorClass);
-
-        return ls;
-    }
-
-    private boolean prepareStage() throws IOException
-    {
-        return true;
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-        List<ColorClass> recentlyAdded = new ArrayList<>(recentlyAdded());
-        List<ColorClass> mainPalettes = new ArrayList<>(mainPalettes());
-
-        try
-        {
-            if (!prepareStage())
-            {
-                return;
-            }
-            for (ColorClass colorClass : recentlyAdded)
-            {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("paletteCard.fxml"));
-                HBox cardBox = fxmlLoader.load();
-                Card cardController = fxmlLoader.getController();
-                cardController.setData(colorClass);
-                cardBox.setStyle("-fx-background-color: transparent;");
-                trendingColorsLayout.getChildren().add(cardBox);
-            }
-            int row = 1, column = 0;
-            for (ColorClass colorClass : mainPalettes)
-            {
-                if (column == 4)
-                {
-                    column = 0;
-                    row++;
-                }
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("paletteCard.fxml"));
-                HBox cardBox = fxmlLoader.load();
-                Card cardController = fxmlLoader.getController();
-                cardController.setData(colorClass);
-                cardBox.setStyle("-fx-background-color: transparent;");
-
-                paletteList.add(cardBox, column++, row);
-                GridPane.setMargin(cardBox, new Insets(10));
-            }
-        }catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-}
-
-
-
- */
